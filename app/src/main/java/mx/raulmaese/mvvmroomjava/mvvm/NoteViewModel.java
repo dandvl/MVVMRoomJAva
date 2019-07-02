eabf79779e6c0993b2bff56b8793375671d40bed
Raúl Maese <rmaese@blacklabs.mx>
2019-07-02 17:07:51 -0500

    Commit inicial.

---
+++ app/src/main/java/mx/raulmaese/mvvmroomjava/mvvm/NoteViewModel.java
@@ -0,0 +1,43 @@
+package mx.raulmaese.mvvmroomjava.mvvm;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import mx.raulmaese.mvvmroomjava.room.Note;
+
+
+public class NoteViewModel extends AndroidViewModel {
+
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    public void insert(Note note) {
+        repository.insert(note);
+    }
+
+    public void update(Note note) {
+        repository.update(note);
+    }
+
+    public void delete(Note note) {
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+}
